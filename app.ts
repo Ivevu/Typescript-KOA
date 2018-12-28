ff2c1ae527f31762b1005b374fb239b0a515c9dd
李文武 <liww@corp.21cn.com>
2018-12-28 17:32:01 +0800

    add: (Problem) 增加问题文件夹，统计出现的问题

--- app.ts
+++ app.ts
@@ -1,26 +1,18 @@
+
 import * as Koa from 'koa';
 import * as Router from "koa-router";
-
-const app = new Koa();
+const path = require('path')
+const server = require('koa-static')
+const app = new Koa()
 const router = new Router();
+var bodyParser = require('koa-bodyparser');
+const staticPath = './webapp'
+app.use(bodyParser());
+app.use(server(path.join(__dirname, staticPath)))
+// app.use(async ctx => {
+//   // ctx.body = ctx.request.body;
+//   ctx.body = ctx.request.query
+// });
 
-app.use(async (ctx, next) => {
-  console.log(1)
-  await next();
-  console.log(2)
-  const rt = ctx.response.get('X-Response-Time');
-  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
-});
-app.use(async (ctx, next) => {
-  const start = Date.now();
-  console.log(3)
-  await next();
-  console.log(4)
-  const ms = Date.now() - start;
-  ctx.set('X-Response-Time', `${ms}ms`);
-});
-app.use(async ctx => {
-  ctx.body = 'Hello World';
-});
-
+app.use(router.routes());
 app.listen(4399);
